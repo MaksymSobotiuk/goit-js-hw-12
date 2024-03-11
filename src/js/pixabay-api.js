e5644f5a4655e65a67ffe0f1310b713e17f2e0b0
MaksymSobotiuk <maxim.4.july.2001@gmail.com>
2024-03-11 10:38:33 +0200

    done

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,19 +1,23 @@
-const KEY = '42798592-8749f29b0ab38d706b4d97822';
+import axios from 'axios';
+
+const KEY = '42676723-0286cb816b5743467714b50d3';
 const BASE_URI = 'https://pixabay.com/api/';
 const IMAGE_TYPE = 'photo';
 const ORIENTATION = 'horizontal'
 const SAFESEARCH = 'true';
 
-export default function getImages(input){
-    const QUERY = input.trim();
-    const LINK = `${BASE_URI}?key=${KEY}&q=${QUERY}&image_type=${IMAGE_TYPE}&orientation=${ORIENTATION}&safesearch=${SAFESEARCH} `;
-
-    return fetch(LINK)
-        .then(response => {
-            if (!response.ok){
-                throw new Error(response.status)
-            }
-            return response.json()
-        })
+export default async function  getImages(query, amount, page){
+    const QUERY = query.trim();
+    const LINK = `${BASE_URI}?key=${KEY}&q=${QUERY}&image_type=${IMAGE_TYPE}&orientation=${ORIENTATION}&safesearch=${SAFESEARCH}&per_page=${amount}&page=${page}`;
+        
+    try {
+        const response = await axios.get(LINK);
+        if (response.status !== 200 ) {
+            throw new Error(response.status);
+        }
+        return response.data;
+    } catch (error) {
+        throw new Error(`Error fetching images: ${error.message}`);
+    }
 
 }
